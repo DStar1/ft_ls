52bfcf6df4ea98b7f2fa0a73fdceef0bbfe492ed
Harrison Smith <hasmith@e1z3r6p2.42.us.org>
2018-04-15 19:54:04 -0700

    fixed permission denied error, but now more leaks

--- srcs/ft_ls.c
+++ srcs/ft_ls.c
@@ -6,7 +6,7 @@
 /*   By: hasmith <hasmith@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/05 20:34:14 by hasmith           #+#    #+#             */
-/*   Updated: 2018/04/08 19:02:27 by hasmith          ###   ########.fr       */
+/*   Updated: 2018/04/15 18:55:20 by hasmith          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -57,8 +57,13 @@ int		listdir_loop(char *path,
 
 	path2 = NULL;
 	path2 = (!one) ? construct_path(path, (*args)->d_name, *args) : path;
+	// ft_printf("NOT CONSTRUCTED: %s\n", path);
 	if (lstat(path2, &file_info) != 0)
+	{
+		(!one) ? free(path2) : 0;
+		ft_printf("PERMISSION DENIED!!!!!!!!\n");
 		return (2);///////////////////////////free?
+	}
 	(*args)->time = file_info.st_mtime;
 	(*args)->nsec = file_info.st_mtimespec.tv_nsec;
 	if (S_ISBLK(file_info.st_mode) || S_ISCHR(file_info.st_mode))
@@ -88,7 +93,7 @@ void	print_reset(char *path, t_bi *tree, t_lsargs *args, int one)
 	args->size_len = ft_intlen(args->size_len);
 	if (args->dir_main && args->first)
 		ft_printf("%s:\n", args->dir_name);
-	if (args->l && !one)
+	if (args->l && !one && !args->error)
 		ft_printf("total %d\n", args->blocks);
 	args->blocks = 0;
 	if (!args->r || one)
@@ -114,19 +119,31 @@ void	listdir_else(char *path,
 					t_lsargs *args,
 					t_bi *tree)
 {
-	if (lstat(path, file_info) == 0)
-	{
+	// // char *tmp;
+	(void)tree;
+	(void)file_info;
+	(void)path;
+	// (args->d_name) ? free(args->d_name) : 0;
+	// args->d_name = ft_strdup(ft_strrchr(path, '/'));
+
+	// if (lstat(path, file_info) == 0)
+	// {
+	// 	ft_printf("PERMISSION DENIED2!!!!!!!!\n");
+	// 	(args->d_name) ? free(args->d_name) : 0;
+	// 	args->d_name = ft_strdup(path);//ft_strrchr(path, '/'));
+	// 	args->one = 1;
+	// 	if (!listdir_loop(path, &args, &tree, 1))
+	// 		print_reset(path, tree, args, 1);
+	// }
+	// else
+	// {
+		// tmp = ft_strrchr(path, '/') + 1;
 		(args->d_name) ? free(args->d_name) : 0;
-		args->d_name = ft_strdup(path);
-		args->one = 1;
-		if (!listdir_loop(path, &args, &tree, 1))
-			print_reset(path, tree, args, 1);
-	}
-	else
-	{
-		perror(ft_strjoin("ft_ls: ", path));
+		args->d_name = (ft_strrchr(path, '/')) ? ft_strdup(ft_strrchr(path, '/') + 1) : ft_strdup(path);//ft_strrchr(path, '/') + 1);
+		// (args->d_name) ? free(args->d_name) : 0;
+		perror(ft_strjoin("ft_ls: ", args->d_name));//ft_strjoin free?
 		args->error = 1;
-	}
+	// }
 }
 
 /*
@@ -147,6 +164,7 @@ void	listdir(char *path, int indent, t_lsargs *args)
 	{
 		while ((entry = readdir(dir)) != NULL)
 		{
+			// ft_printf("FILE?FOLDER: %s\n", path);
 			args->major = 0;
 			args->minor = 0;
 			(args->d_name) ? free(args->d_name) : 0;
--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: hasmith <hasmith@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/04/01 21:44:20 by hasmith           #+#    #+#             */
-/*   Updated: 2018/04/08 18:38:13 by hasmith          ###   ########.fr       */
+/*   Updated: 2018/04/15 19:48:18 by hasmith          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -141,5 +141,7 @@ int		main(int ac, char **av)
 	}
 	main_helper(&args);
 	(ac > 2) ? free_array(args.all_paths) : 0;//free(args.all_paths[0]);
+	// while (1)
+	// 	;
 	return (0);
 }
